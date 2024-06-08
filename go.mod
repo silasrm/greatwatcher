b329d6309f60981398360bb5907a04cbb29151e6
Silas Ribas Martins <silasrm@gmail.com>
2024-06-08 19:23:08 -0300

    Inicio funcional

---
+++ go.mod
@@ -0,0 +1,18 @@
+module app
+
+go 1.19
+
+require (
+	github.com/dustin/go-humanize v1.0.1
+	github.com/joho/godotenv v1.5.1
+	github.com/radovskyb/watcher v1.0.7
+	github.com/rs/zerolog v1.33.0
+	golang.org/x/net v0.26.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,412 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/xml"
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"github.com/joho/godotenv"
+	"github.com/radovskyb/watcher"
+	"github.com/rs/zerolog"
+	"golang.org/x/net/html/charset"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime/debug"
+	"strings"
+	"time"
+)
+
+var (
+	buildInfo, _ = debug.ReadBuildInfo()
+
+	logger = zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+		FormatLevel: func(i interface{}) string {
+			return strings.ToUpper(fmt.Sprintf("[%s]", i))
+		},
+		FormatMessage: func(i interface{}) string {
+			return fmt.Sprintf("| %s |", i)
+		},
+		FormatCaller: func(i interface{}) string {
+			return filepath.Base(fmt.Sprintf("%s", i))
+		},
+		PartsExclude: []string{
+			zerolog.TimestampFieldName,
+		},
+	}).
+		Level(zerolog.TraceLevel).
+		With().
+		Timestamp().
+		Caller().
+		Int("pid", os.Getpid()).
+		Str("go_version", buildInfo.GoVersion).
+		Logger()
+)
+
+func fileExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func folderExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+func extractFile(data Resultado, outputDir string) string {
+	//sDec, _ := b64.StdEncoding.DecodeString(data.Pdf)
+	//logger.Println(string(sDec))
+	//logger.Println()
+	dec, err := base64.StdEncoding.DecodeString(data.Pdf)
+	if err != nil {
+		panic(err)
+	}
+
+	filename := outputDir + data.Prescricao + ".pdf"
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if _, err := f.Write(dec); err != nil {
+		panic(err)
+	}
+	if err := f.Sync(); err != nil {
+		panic(err)
+	}
+
+	return filename
+}
+
+func getXml(path string) (Resultado, error) {
+	// Open our xmlFile
+	xmlFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		logger.Println(err)
+	}
+
+	logger.Info().Msgf("\t-> Abrindo %s...", path)
+	// defer the closing of our xmlFile so that we can parse it later on
+	defer xmlFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, err := io.ReadAll(xmlFile)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	resultado := Resultado{}
+
+	reader := bytes.NewReader(byteValue)
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReaderLabel
+	err = decoder.Decode(&resultado)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	return resultado, err
+}
+
+func UploadFile(url string, params map[string]string, files ...File) (*bytes.Buffer, error) {
+	var (
+		buf = new(bytes.Buffer)
+		w   = multipart.NewWriter(buf)
+	)
+
+	for _, f := range files {
+		part, err := w.CreateFormFile(f.Name, filepath.Base(f.Extension))
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = part.Write(f.File)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for key, val := range params {
+		_ = w.WriteField(key, val)
+	}
+
+	err := w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", w.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	//if err != nil {
+	//	return []byte{}, err
+	//}
+	//defer res.Body.Close()
+	//
+	//cnt, err := io.ReadAll(res.Body)
+	//if err != nil {
+	//	return []byte{}, err
+	//}
+	//
+	//return cnt, nil
+
+	if err != nil {
+		return nil, err
+	}
+
+	body := &bytes.Buffer{}
+	_, err = body.ReadFrom(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp.Body.Close()
+	//fmt.Println(resp.StatusCode)
+	//fmt.Println(resp.Header)
+	//fmt.Println(body)
+
+	return body, nil
+}
+
+func MoveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %v", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open dest file: %v", err)
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return fmt.Errorf("Couldn't copy to dest from source: %v", err)
+	}
+
+	inputFile.Close() // for Windows, close before trying to remove: https://stackoverflow.com/a/64943554/246801
+
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't remove source file: %v", err)
+	}
+	return nil
+}
+
+type Prescricao struct {
+	XMLName          xml.Name `xml:"prescricao"`
+	NumeroPrescricao string   `xml:"NR_PRESCRICAO"`
+	DataPrescricao   string   `xml:"DT_PRESCRICAO"`
+	NomePaciente     string   `xml:"NM_PACIENTE"`
+	DataNascimento   string   `xml:"DT_NASCIMENTO"`
+	Sexo             string   `xml:"IE_SEXO"`
+	Logradouro       string   `xml:"DS_ENDERECO"`
+	Numero           string   `xml:"NR_ENDERECO"`
+	Complemento      string   `xml:"DS_COMPLEMENTO"`
+	Bairro           string   `xml:"DS_BAIRRO"`
+	Municipio        string   `xml:"DS_MUNICIPIO"`
+	Estado           string   `xml:"SG_ESTADO"`
+	Cep              string   `xml:"CD_CEP"`
+	Telefone         string   `xml:"NR_TELEFONE"`
+	Cpf              string   `xml:"NR_CPF"`
+	Unidade          string   `xml:"CD_UNIDADE"`
+	Crm              string   `xml:"NR_CRM"`
+	Medico           string   `xml:"NM_MEDICO"`
+	CodigoConvenio   string   `xml:"CD_CONVENIO"`
+	Convenio         string   `xml:"DS_CONVENIO"`
+	Plano            string   `xml:"DS_PLANO_CONVENIO"`
+	NumeroProntuario string   `xml:"NR_PRONTUARIO"`
+	Urgencia         string   `xml:"IE_URGENCIA"`
+	CodigoOrigem     string   `xml:"CD_ORIGEM"`
+	Origem           string   `xml:"DS_ORIGEM"`
+	Usuario          string   `xml:"NM_USUARIO"`
+	Exames           Exames   `xml:"EXAMES"`
+}
+
+type Exames struct {
+	XMLName xml.Name `xml:"EXAMES"`
+	Exame   string   `xml:"EXAME"`
+}
+
+type Resultado struct {
+	XMLName       xml.Name `xml:"prescricao"`
+	Prescricao    string   `xml:"NR_PRESCRICAO"`
+	Exame         string   `xml:"EXAME"`
+	DataLiberacao string   `xml:"DATA_LIBERACAO"`
+	Pdf           string   `xml:"PDF"`
+	PdfPath       string
+}
+
+type File struct {
+	Name      string
+	Extension string
+	File      []byte
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error().Msg("Error loading .env file")
+	}
+
+	resultPath := os.Getenv("RESULT_PATH")
+	resultDispatchedPath := os.Getenv("RESULT_DISPATCHED_PATH")
+	serverUploadUrl := os.Getenv("SERVER_UPLOAD_URL")
+	userId := os.Getenv("USER_ID")
+	appToken := os.Getenv("APP_TOKEN")
+
+	if !folderExists(resultPath) {
+		logger.Error().Msgf("Folder does not exist: %s", resultPath)
+	} else {
+		logger.Info().Msgf("Folder found: %s", resultPath)
+	}
+
+	w := watcher.New()
+
+	// SetMaxEvents to 1 to allow at most 1 event's to be received
+	// on the Event channel per watching cycle.
+	//
+	// If SetMaxEvents is not set, the default is to send all events.
+	w.SetMaxEvents(1)
+
+	// Only notify rename and move events.
+	//w.FilterOps(watcher.Rename, watcher.Move)
+	w.FilterOps(watcher.Rename, watcher.Move, watcher.Create)
+
+	// Only files that match the regular expression during file listings
+	// will be watched.
+	r := regexp.MustCompile("^*.xml$")
+	w.AddFilterHook(watcher.RegexFilterHook(r, false))
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				if !event.IsDir() && event.Path != "-" {
+					size := uint64(event.Size())
+					logger.Info().Msgf("Evento: %s", event.Op)
+					logger.Info().Msgf("\tArquivo: %s", event.Name())
+					logger.Info().Msgf("\tTamanho: %s", humanize.Bytes(size))
+					logger.Info().Msgf("\tCaminho: %s", event.Path)
+
+					r, err := getXml(event.Path)
+					if err != nil {
+						logger.Error().Msg("\t\t-> Error loading .env file")
+					}
+
+					logger.Info().Msgf("\t\t-> ID: %s", r.Prescricao)
+
+					if len(r.Pdf) > 0 {
+						//logger.Info().Msgf("\t\tPDF: %s", r.Pdf)
+						logger.Info().Msg("\t\tCOM PDF!")
+
+						pdfPath := extractFile(r, resultPath)
+
+						if len(pdfPath) > 0 {
+							r.PdfPath = pdfPath
+						}
+
+						logger.Info().Msgf("\t\t--> Extraido em: %s", r.PdfPath)
+
+						extraParams := map[string]string{
+							"id":         r.Prescricao,
+							"exame":      r.Exame,
+							"data":       r.DataLiberacao,
+							"usuario_id": userId,
+							"token":      appToken,
+						}
+
+						fileContent, err := os.Open(r.PdfPath)
+						// if we os.Open returns an error then handle it
+						if err != nil {
+							logger.Println(err)
+						}
+
+						fileContentBytes, err := io.ReadAll(fileContent)
+						if err != nil {
+							logger.Println(err)
+						}
+
+						file := File{
+							Name:      event.Name(),
+							Extension: "pdf",
+							File:      fileContentBytes,
+						}
+
+						_, err = UploadFile(serverUploadUrl, extraParams, file)
+						if err != nil {
+							logger.Println(err)
+						}
+
+						err = MoveFile(event.Path, resultDispatchedPath+filepath.Base(event.Path))
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						logger.Info().Msgf("\t\t--> Movido de %s para %s", event.Path, resultDispatchedPath+filepath.Base(event.Path))
+
+						err = MoveFile(r.PdfPath, resultDispatchedPath+filepath.Base(r.PdfPath))
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						logger.Info().Msgf("\t\t--> Movido de %s para %s", r.PdfPath, resultDispatchedPath+filepath.Base(r.PdfPath))
+						//logger.Println(result)
+					} else {
+						logger.Info().Msg("\t\tSem PDF!")
+					}
+				}
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch this folder for changes.
+	if err := w.Add(resultPath); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print a list of all of the files and folders currently
+	// being watched and their paths.
+	for path, f := range w.WatchedFiles() {
+		logger.Info().Msgf("%s: %s\n", path, f.Name())
+	}
+
+	logger.Println()
+
+	// Trigger 2 events after watcher started.
+	go func() {
+		w.Wait()
+		w.TriggerEvent(watcher.Create, nil)
+		w.TriggerEvent(watcher.Remove, nil)
+	}()
+
+	// Start the watching process - it'll check for changes every 100ms.
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
